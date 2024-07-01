c354f75b8db6f6e9c838d722cb4e0b800f062eb6
0910QiChen <0910qichen@gmail.com>
2024-07-01 15:32:05 -0400

    Add project files.

---
+++ MVCProjectCodeFirst/DAL/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using MVCProjectCodeFirst.Models;
+
+namespace MVCProjectCodeFirst.DAL
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() : base ("UserContext")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
